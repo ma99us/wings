28a1d1475ec81c08827e7639771768251f7ba54c
ma99us@gmail.com <qweRTY123>
2021-09-28 17:11:42 -0300

    For security purposes; do not check this file git history :-)!

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,4 +1,6 @@
-// const HOST_LOCATION = '';                      // <- release
-export const HOST_LOCATION = 'gerdov.com';        // <- development
-export const HOST_API_KEY = 'WNGS53cr3tK3y';
-export const HOST_DB_NAME = 'wingsDB';
+/**
+ * This is only a template. Ask Mike to get some real values for this
+ */
+export const HOST_LOCATION = 'TODO: your domain here';
+export const HOST_API_KEY = 'TODO: mike-db API password here';
+export const HOST_DB_NAME = 'TODO: mike-db DB name here';
